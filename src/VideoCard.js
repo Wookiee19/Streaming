81fb214e3f5a52680edeb33484580bbd60afcb70
Wookiee19 <93146509+Wookiee19@users.noreply.github.com>
2022-03-04 18:28:11 +0530

    youtube

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -113,7 +113,7 @@ const VideoCard = forwardRef(({ movieList, id }, ref) => {
                     loading="lazy"
                     width="550"
                     height="400"
-                    src={`https://www.youtube.com/embed/4UZrsTqkcW4`}
+                    src={`https://www.youtube.com/embed/mqqft2x_Aa4`}
                     title="YouTube video player"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
